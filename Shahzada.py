9bb6aaee5c14d65c6c1d56f0379e515e1f81a929
Bigrashahzada <99671642+Bigrashahzada@users.noreply.github.com>
2022-02-21 02:25:09 +0500

    Update Shahzada.py
    https://github.com/Bigrashahzada/love.git
    cd love
    Python2 Shahzada.py

--- Shahzada.py
+++ Shahzada.py
@@ -1,4 +1,4 @@
-# Author Yunus#!/usr/bin/python2
+# Author zaid#!/usr/bin/python2
 #coding=utf-8
 import os,sys,time,datetime,random,hashlib,re,threading,json,urllib,cookielib,getpass
 os.system('rm -rf .txt')
